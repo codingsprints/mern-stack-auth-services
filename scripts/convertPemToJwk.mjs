e69d1549a2f1302d3745e38e26202ca27c1e5867
codingsprints <codingsprint31@gmail.com>
2025-08-23 15:23:56 +0530

    convert Pem To Jwk in application

---
+++ scripts/convertPemToJwk.mjs
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import rsaPemToJwk from 'rsa-pem-to-jwk';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const privateKey = fs.readFileSync(
+  path.resolve(__dirname, '../certs/private.pem'),
+  'utf-8',
+);
+
+const jwk = rsaPemToJwk(privateKey, { use: 'sig', alg: 'RS256' }, 'public');
+
+// Add Key ID (kid)
+jwk.kid = createHash('sha256').update(privateKey).digest('hex').slice(0, 10);
+
+// Final JWKS structure
+const jwks = { keys: [jwk] };
+
+// Write it to .well-known/jwks.json
+const outputPath = path.resolve(__dirname, '../public/.well-known/jwks.json');
+fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+fs.writeFileSync(outputPath, JSON.stringify(jwks, null, 2));
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import { configENV } from './config/config';
 import authRouter from './routes/authRouter';
 import { globalErrorHandler } from './middleware/globalErrorHandler';
+import path from 'path';
 
 const app = express();
 app.use(express.static('public'));
@@ -10,6 +11,16 @@ app.use(cookieParser());
 app.use(express.json());
 
 console.log('env -->', configENV.port);
+
+// app.use(
+//   '/.well-known',
+//   express.static(path.join(__dirname, 'public/.well-known')),
+// );
+
+app.use(
+  '/.well-known',
+  express.static(path.join(__dirname, '../public/.well-known')),
+);
 
 app.get('/', async (req, res) => {
   console.log('hello');
